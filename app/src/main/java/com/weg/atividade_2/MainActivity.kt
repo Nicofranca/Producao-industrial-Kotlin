567b158ffa975c463acd1225c6de9114848ba8af
Nicollas FranÃca <nicollas_franca@estudante.sesisenai.org.br>
2026-06-23 19:06:37 -0300

    feat: finaliza actividy_main

--- app/src/main/java/com/weg/atividade_2/MainActivity.kt
+++ app/src/main/java/com/weg/atividade_2/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.weg.atividade_2
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val edtMaquina = findViewById<EditText>(R.id.edtMaquina)
+        val edtProduzidas = findViewById<EditText>(R.id.edtProduzidas)
+
     }
 }
